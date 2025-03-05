2fe59237bde829e46c2f718a0fbd2a25d61293a7
Kaycee Nuel <heykayceee@gmail.com>
2025-03-05 22:56:54 +0100

    tasting buffer

--- buffer_test.go
+++ buffer_test.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 )
 
-func justTestingForNow(t *testing.T) {
-
+func TestBufferWrite(t *testing.T) {
+	var b bytes.Buffer
+	n, err := b.Write([]byte("Hello"))
+	if err != nil || n != 5 || b.String() != "hello" {
+		t.Errorf("Write failed: n=%d, err=%v, got=%s", n, err, b.String())
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kayceenuel/interfaces
+
+go 1.23.4
