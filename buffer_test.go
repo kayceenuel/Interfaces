73134b4adbd30981c53a01446b2e70554b077e02
Kaycee Nuel <heykayceee@gmail.com>
2025-03-06 21:43:17 +0100

    Test: Added test for io write & Read

--- buffer_test.go
+++ buffer_test.go
@@ -7,8 +7,17 @@ import (
 
 func TestBufferWrite(t *testing.T) {
 	var b bytes.Buffer
-	n, err := b.Write([]byte("Hello"))
+	n, err := b.Write([]byte("hello"))
 	if err != nil || n != 5 || b.String() != "hello" {
 		t.Errorf("Write failed: n=%d, err=%v, got=%s", n, err, b.String())
+	}
+}
+
+func TestBufferRead(t *testing.T) {
+	b := bytes.NewBufferString("hello")
+	p := make([]byte, 5)
+	n, err := b.Read(p)
+	if err != nil || n != 5 || string(p) != "hello" {
+		t.Errorf("Read failed: n=%d, err=%v, got=%s", n, err, p)
 	}
 }
