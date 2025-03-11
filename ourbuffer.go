cc4c0b52f11531fc40c7c7dae4a3a87b5c1841f1
Kaycee Nuel <heykayceee@gmail.com>
2025-03-11 23:07:23 +0100

    modified code and comments

--- ourbuffer.go
+++ ourbuffer.go
@@ -2,37 +2,64 @@ package main
 
 import "io"
 
-// Simple Buffer that holds bytes in memory.
-//you can write byte to it or read byte from it. Just like Input stuff and output stuff.
+// OurByteBuffer is a custom implementation of a buffer that stores bytes.
+// It implements both io.Reader and io.Writer interfaces.
+//
+// Limitations:
+// - Thread Safety: Not safe for concurrent use without external synchronization
+// - Memory: Keeps all data in memory until explicitly cleared
+// - Size: Limited only by available system memory
 type OurByteBuffer struct {
-	data []byte //slice of byte that holds all the data (io data.)
+	data    []byte // Stores all the buffer content
+	readPos int    // Tracks how much has been read
 }
 
-// NewOurByteBuffer creates a new buffer with some starting text.
-// It’s like making a new bucket and pouring in some initial water.
-func NewOurByteBuffer(initial string) *OurByteBuffer {
-	return &OurByteBuffer{data: []byte(initial)} // Turn the string into bytes and put it in the bucket.
-}
-
-// Write adds new bytes to the buffer.
-// It’s like pouring more water into the bucket.
-func (b *OurByteBuffer) Write(p []byte) (n int, err error) {
-	b.data = append(b.data, p...) // Add the new bytes to the end of the bucket’s contents.
-	return len(p), nil            // Tell how much was added (size of p) and say “no problems” (nil error).
-}
-
-// Read take bytes out of the buffer and puts them into your container.
-func (b *OurByteBuffer) Read(p []byte) (n int, err error) {
-	if len(b.data) == 0 { // If the bucket is empty...
-		return 0, io.EOF // Say “nothing scooped” (0) and “bucket’s dry” (EOF error).
+// NewOurByteBuffer creates a new buffer with the given initial data
+func NewOurByteBuffer(initialData []byte) *OurByteBuffer {
+	// Create and return a new buffer containing the initial data
+	return &OurByteBuffer{
+		data:    append([]byte{}, initialData...), // Make a copy of the input data
+		readPos: 0,                                // Start with nothing read
 	}
-	n = copy(p, b.data) // Scoop as much as fits into your cup (p) from the bucket.
-	b.data = b.data[n:] // Remove what you scooped from the bucket.
-	return n, nil       // Tell how much you scooped and say “no problems”.
 }
 
-// Bytes shows you everything currently in the buffer.
-// It’s like looking into the bucket to see what’s there.
+// Write adds more bytes to the end of the buffer
+// Implements the io.Writer interface
+func (b *OurByteBuffer) Write(p []byte) (n int, err error) {
+	// Add the new bytes to the end of our data
+	b.data = append(b.data, p...)
+	// Return the number of bytes we added (all of them) and no error
+	return len(p), nil
+}
+
+// Read gets bytes from the buffer into the provided slice
+// Implements the io.Reader interface
+func (b *OurByteBuffer) Read(p []byte) (n int, err error) {
+	// If we've already read everything, return EOF
+	if b.readPos >= len(b.data) {
+		return 0, io.EOF
+	}
+
+	// Calculate how many bytes we can read
+	// (either what's left or the size of the destination slice, whichever is smaller)
+	bytesRemaining := len(b.data) - b.readPos
+	bytesToRead := len(p)
+	if bytesToRead > bytesRemaining {
+		bytesToRead = bytesRemaining
+	}
+
+	// Copy the bytes from our buffer to the destination slice
+	n = copy(p, b.data[b.readPos:b.readPos+bytesToRead])
+
+	// Update our reading position
+	b.readPos += n
+
+	// Return the number of bytes read and no error
+	return n, nil
+}
+
+// Bytes returns a slice containing all the bytes in the buffer
 func (b *OurByteBuffer) Bytes() []byte {
-	return b.data //Just hand over the bucket’s contents.
+	// Return a copy of all the data, regardless of read position
+	return append([]byte{}, b.data...)
 }
