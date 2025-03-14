b564ef16158d68ecb602791d4e2e91491d4663b0
Kaycee Nuel <heykayceee@gmail.com>
2025-03-14 09:33:43 +0100

    added Read and Write demo

--- main.go
+++ main.go
@@ -12,4 +12,10 @@ func main() {
 	buffer := NewOurByteBuffer([]byte("Hello"))
 	fmt.Printf("Initial buffer: %s\n", buffer.Bytes())
 
+	// Write to the buffer
+	buffer.Write([]byte(", world"))
+	fmt.Printf("After writing: %s\n", buffer.Bytes())
+
+	//Read to the buffer
+
 }
